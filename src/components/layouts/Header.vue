552fc44f444b0b205b38d6205b4f451e0e459ceb
tottranv <dev.tottranvan@gmail.com>
2024-09-30 13:28:55 +0700

    impl ant layout

---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <a-layout-header class="bg-white p-0" />
+</template>
+<script>
+export default {
+    name: 'HeaderComponent'
+}
+</script>
---
+++ src/components/layouts/MainLayout.vue
@@ -0,0 +1,39 @@
+<template>
+    <a-layout id="components-layout-demo-side" style="min-height: 100vh">
+        <Sidebar />
+        <a-layout>
+            <Header />
+            <a-layout-content style="margin: 0 16px">
+                <a-breadcrumb style="margin: 16px 0">
+                    <a-breadcrumb-item>Product</a-breadcrumb-item>
+                    <a-breadcrumb-item>Laptop</a-breadcrumb-item>
+                </a-breadcrumb>
+                <div :style="{ padding: '24px', background: '#fff', minHeight: '360px' }">
+                    <router-view />
+                </div>
+            </a-layout-content>
+            <a-layout-footer style="text-align: center">
+                Ant Design ©2018 Created by Ant UED
+            </a-layout-footer>
+        </a-layout>
+    </a-layout>
+</template>
+<script>
+import Header from './Header.vue';
+import Sidebar from './Sidebar.vue';
+
+export default {
+    components: {
+        Sidebar,
+        Header,
+    },
+};
+</script>
+
+<style>
+#components-layout-demo-side .logo {
+    height: 32px;
+    background: rgba(255, 255, 255, 0.2);
+    margin: 16px;
+}
+</style>
