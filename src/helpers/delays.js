ad3dc01643285e99034839720bd438668d52452a
tottranv <dev.tottranvan@gmail.com>
2024-10-05 11:19:21 +0700

    countdown reuse

--- src/helpers/delays.js
+++ src/helpers/delays.js
@@ -1,3 +1,5 @@
+import { message } from "ant-design-vue";
+
 export const debounce = (func, wait = 250) => {
     let timeoutId;
     return (...args) => {
@@ -5,3 +7,17 @@ export const debounce = (func, wait = 250) => {
         timeoutId = setTimeout(() => func.call(this, ...args), wait);
     }; 
 }
+
+export const showCountdownMessage = (time = 3, cb) => {
+    let countdown = time;
+    message.loading('Redirect to login after few seconds', 0);
+
+    const interval = setInterval(() => {
+        countdown -= 1;
+        if (countdown === 0) {
+            clearInterval(interval);
+            message.destroy();
+            cb();
+        }
+    }, 1000);
+};
