0d3713b7fad9d32b51f652fb50e8a5be99931d32
tottranv <dev.tottranvan@gmail.com>
2024-10-13 20:33:34 +0700

    impl pwa

---
+++ public/service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('my-cache').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/android-icon-192x192.png',
+                '/ms-icon-310x310.png',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
